790629f1d92a199b07ee3565fd43e296f2218b80
Robin Wen <blockxyz@gmail.com>
2022-09-23 17:05:42 +0800

    Initial commit

---
+++ snapshots/beacon_snapshots.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copyright © 2022 ExinPool <robin@exin.one>
+#
+# Distributed under terms of the MIT license.
+#
+# Desc: Beacon Nodes Snaptshots.
+# User: Robin@ExinPool
+# Date: 2022-05-23
+# Time: 17:04:59
+
+nodes=(`cat nodes.log`)
+
+curdate=`date "+%F-%H-%M"`
+
+cd /data/monitor/exinpool/Ethereum && mkdir -p data/$curdate
+dir="/data/monitor/exinpool/Ethereum/data/$curdate"
+
+for node in "${nodes[@]}"
+do
+    file="$dir/$curdate.log"
+    balance=`curl --silent https://beaconscan.com/main/validator/0x$i | grep "est APR of" | awk '{print $1}'`
+    apr=`curl --silent https://beaconscan.com/main/validator/0x$i | grep "est APR of" | awk -F'(' '{print $2}' | awk -F'%' '{print $1}' | awk '{print $4}'`
+    echo "$node $balance $apr" >> $file
+done
