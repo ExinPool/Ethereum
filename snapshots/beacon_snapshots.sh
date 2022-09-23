be6371e5241c697fd1630bf4259e0983302572cd
Robin Wen <blockxyz@gmail.com>
2022-09-23 17:52:59 +0800

    Update to API request

--- snapshots/beacon_snapshots.sh
+++ snapshots/beacon_snapshots.sh
@@ -9,6 +9,7 @@
 # Date: 2022-05-23
 # Time: 17:04:59
 
+apikey=$1
 nodes=(`cat nodes.log`)
 
 curdate=$(date '+%Y-%m-%d')
@@ -19,7 +20,7 @@ dir="/data/monitor/exinpool/Ethereum/data/$curdate"
 for node in "${nodes[@]}"
 do
     file="$dir/snaptshots-$curdate.log"
-    balance=`curl -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" --silent https://beaconscan.com/main/validator/0x$node | grep "est APR of" | awk '{print $1}'`
-    apr=`curl -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" --silent https://beaconscan.com/main/validator/0x$node | grep "est APR of" | awk -F'(' '{print $2}' | awk -F'%' '{print $1}' | awk '{print $4}'`
-    echo "$node $balance $apr" >> $file
+    balanceBig=`curl --silent -X 'GET' 'https://beaconcha.in/api/v1/validator/$node?apikey=$apikey' -H 'accept: application/json' | jq | grep -w balance | awk -F':' '{print $2}' | sed "s/ //g" | sed "s/,//g"`
+    balance=`echo "scale=9; $balanceBig/1000000000" | bc`
+    echo "$node $balance" >> $file
 done
